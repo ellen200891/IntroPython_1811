14fe1a530314a55c4310c1d0a6862a2a4f48b932
ellen <alena.kushnir17@gmail.com>
2020-11-28 14:15:17 +0200

    Home work 3

--- HW3.py
+++ HW3.py
@@ -2,7 +2,7 @@
 # 1) У вас есть переменная value, тип - int. Написать тернарный оператор для переменной new_value по такому правилу:
 # если value меньше 100, то new_value равно половине значения value, в противном случае - противоположне value число
 value = int(input("Введите число "))
-new_value = value/2 if value < 100  else -value
+new_value = value / 2 if value < 100 else -value
 print(new_value)
 
 ###############################################################################
@@ -20,7 +20,7 @@ new_value = True if value < 100 else False
 print(new_value)
 
 ##############################################################################
-#4) У вас есть переменная my_str, тип - str. Заменить в my_str все маленькие буквы на большие.
+# 4) У вас есть переменная my_str, тип - str. Заменить в my_str все маленькие буквы на большие.
 my_str = 'HeLlO,,,  wOrLd!'
 high_str = my_str.upper()
 print(high_str)
@@ -36,7 +36,7 @@ print(low_str)
 # Пример: было - "qwer", стало - "qwerqwer". Если длинна не меньше 5, то оставить строку как есть.
 my_str = input("Напишите текст: ")
 len_str = len(my_str)
-my_str = my_str*2 if len_str < 5 else my_str
+my_str = my_str * 2 if len_str < 5 else my_str
 print(my_str)
 
 ##############################################################################
@@ -45,7 +45,7 @@ print(my_str)
 my_str = input("Напишите текст: ")
 len_str = len(my_str)
 str_my = my_str[::-1]
-my_str = str_my if len_str <5 else my_str
+my_str = str_my if len_str < 5 else my_str
 print(my_str)
 
 ##############################################################################
@@ -59,12 +59,12 @@ for symbol in my_str:
 # 9) У вас есть переменная my_str, тип - str. Вывести на экран все символы из этой строки, которые не являются буквой или цифрой.
 my_str = 'HeLlO,,,  wOrLd!'
 for symbol in my_str:
-     if not symbol.isalnum():
-         print(symbol)
+    if not symbol.isalnum():
+        print(symbol)
 
 ##############################################################################
 # 10) У вас есть переменная my_str, тип - str. Вывести на экран все символы из этой строки, которые не являются буквой или цифрой и не пробел.
 my_str = 'HeLlO,,,  wOrLd!'
 for symbol in my_str:
-      if not symbol.isalnum() and not symbol == " ":
-          print(symbol)
+    if not symbol.isalnum() and not symbol == " ":
+        print(symbol)
