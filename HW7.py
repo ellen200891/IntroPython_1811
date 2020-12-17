a93210661b26c986f76df26004241dc1ce28485f
ellen <alena.kushnir17@gmail.com>
2020-12-17 12:44:48 +0200

    Home work 5

---
+++ HW7.py
@@ -0,0 +1,124 @@
+# 1) Создать список из 20 случайных целых чисел в диапазоне от 1 до 100.
+# Задание можно выполнить и через обычный цикл и через генератор списков.
+
+import random
+
+new_list = [random.randint(1, 100) for number in range(20)]
+print(new_list)
+
+
+###########################################################################
+# 2) Создать словарь triangle в который записать точки A B C (ключи),
+# и их координаты - кортежи (значения), созданные случайным образом с помощью модуля random в диапазоне от -10 до 10 по каждой оси.
+
+triangle = {"A": (random.randint(-10, 10),
+                  random.randint(-10, 10),
+                  random.randint(-10, 10)),
+            "B": (random.randint(-10, 10),
+                  random.randint(-10, 10),
+                  random.randint(-10, 10)),
+            "C": (random.randint(-10, 10),
+                  random.randint(-10, 10),
+                  random.randint(-10, 10))}
+print(triangle)
+############################################################################
+
+# 3) Создать функцию my_print, которая принимает в виде параметра строку и печатает ее
+# с тремя символами * вначале и в конце строки.
+# Пример:
+# my_str = 'I'm the string'
+# Печатает ***I'm the string***
+
+
+my_str = "I'm the string"
+star = "***"
+print(f"{star}{my_str}{star}")
+
+###########################################################################
+# 4) Дан список словарей persons в формате [{"name": "John", "age": 15}, ... ,{"name": "Jack", "age": 45}]
+# а) Напечатать имя самого молодого человека. Если возраст совпадает - напечатать все имена.
+# б) Напечатать самое длинное имя. Если длина имени совпадает - напечатать все имена.
+# в) Посчитать среднее количество лет всех людей из списка.
+
+persons = [{"name": "Luce", "age": 76}, {"name": "Nick", "age": 17}, {"name": "Alice", "age": 92}]
+names_ages = [(person["name"], person["age"]) for person in persons]
+min_age = 100
+name_min_age = []
+list_person = []
+sum_start = 0
+max_len_name = 0
+max_name = []
+
+for person in persons:
+    list_person.append(person['age'])
+    if person["age"] < min_age:
+        min_age = person["age"]
+    if len(person["name"]) > max_len_name:
+        max_len_name = len(person["name"])
+
+##############   A   ##############
+
+for person in persons:
+    if min_age == person['age']:
+        name_min_age.append(person["name"])
+name_min_age = ', '.join(name_min_age)
+print(name_min_age)
+
+
+##############  Б    ##############
+for person in persons:
+    if max_len_name == len(person['name']):
+        max_name.append(person['name'])
+max_name = ', '.join(max_name)
+print(max_name)
+
+
+
+##############  В    ##############
+for number in list_person:
+    people = len(list_person)
+    sum_start+=number
+    midl_age = sum_start // people
+print(midl_age)
+
+###########################################################################
+# 5) Даны два словаря my_dict_1 и my_dict_2.
+# а) Создать список из ключей, которые есть в обоих словарях.
+# б) Создать список из ключей, которые есть в первом, но нет во втором словаре.
+# в) Создать новый словарь из пар {ключ:значение}, для ключей, которые есть в первом, но нет во втором словаре.
+# г) Объединить эти два словаря в новый словарь по правилу:
+# если ключ есть только в одном из двух словарей - поместить пару ключ:значение,
+# если ключ есть в двух словарях - поместить пару {ключ: [значение_из_первого_словаря, значение_из_второго_словаря]}
+
+
+my_dict_1 = {"street": "Lenina",
+             "home": '5',
+             "flat": "1"}
+my_dict_2 = {"squer": '12',
+             "flat": "2"}
+
+##############  А, Б    ##############
+first_keys = set(my_dict_1.keys())
+first_keys1 = first_keys.copy()
+second_keys = set(my_dict_2.keys())
+second_keys2 = second_keys.copy()
+first_keys1.update(second_keys2)
+first_keys.difference_update(second_keys)
+first_keys1 = list(first_keys1)
+first_keys = list(first_keys)
+print(first_keys1)
+print(first_keys)
+
+##############  В    ##############
+
+my_dict_3 = dict((key, my_dict_1[key]) for key in my_dict_1 if key not in my_dict_2)
+print(my_dict_3)
+
+##############  C    ##############
+my_dict_4 = {}
+for element in my_dict_1.items():
+    if element[0] not in my_dict_2:
+        my_dict_4[element[0]] = element[1]
+    else:
+        my_dict_4[element[0]] = [element[1], my_dict_2[element[0]]]
+print(my_dict_4)
---
+++ Lesson8.py
@@ -0,0 +1,84 @@
+# параметры функций
+# *arg, **kwargs
+# работа с фаилами
+# time,sys, os, os.path
+# import random
+# import string
+import os
+
+
+with open("C:/Users/Пользователь/Desktop/Hillel1/lesson7.txt","r", encoding="utf-8") as txt_file:
+    data = []
+    for line in txt_file.readlines():
+        data.append(line.strip())
+
+print(len(data), type(data))
+for line in data:
+    print(line)
+
+
+
+
+# print(string.ascii_lowercase)
+# DEBUG_MOD = True
+#
+# def create_random_str(str_len, debug_mod=False):
+#     alphabet = list(string.ascii_lowercase)
+#     random.shuffle(alphabet)
+#     random_str = "".join(alphabet[:str_len])
+#     if debug_mod:
+#         print(random_str)
+#     return random_str
+#
+# def testing_args(q, w, e, r, t, y, debug_mod=DEBUG_MOD):
+#     if debug_mod:
+#         print(q, w, e, r, t, y)
+# random_str = create_random_str(10, debug_mod=DEBUG_MOD)
+#
+# testing_args = (1,2,3,4,5,6)
+
+
+
+# def testing_args(q, w, e, r, t, y):
+#     print(q, w, e, r, t, y)
+#
+# def testing_default_values(first, second):
+#     result = first + second
+#     return result
+#
+# result = testing_default_values(12, 10)
+# print(result)
+
+
+
+#testing_args("q", "w", r=1, y=2, t=0, e="e")
+
+
+
+
+
+# def crate_random_point(min_limit, max_limit):
+#     # print("min_limit", "max_limit", min_limit, max_limit)
+#     point = (random.randint(min_limit, max_limit),
+#              random.randint(min_limit, max_limit),
+#              random.randint(min_limit, max_limit))
+#     return point
+#
+#
+# def crate_line_segment(stop, start):
+#     line_segment = {"A": crate_random_point(start, stop),
+#                     "B": crate_random_point(start, stop)}
+#     return line_segment
+
+
+# point_max = crate_random_point(-100, 100)
+# point = crate_random_point(-10, 10)
+
+# print(point_max)
+
+# AB = crate_line_segment(10, 100)   ######### Позиционные аргументы #######
+# print(AB)
+# stop = 100
+# start = 10
+# AB = crate_line_segment(stop=stop, start=start)  # именованные аргументы
+# print(AB)
