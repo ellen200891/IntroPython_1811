ea44682fea01d79f47ea9004c17e54d403535bf1
ellen <alena.kushnir17@gmail.com>
2020-12-29 15:42:22 +0200

    Home work 5

--- HW10.py
+++ HW10.py
@@ -12,21 +12,20 @@ def open_json_data():
 dict_list = open_json_data()
 
 # 2. Написать функцию сортировки данных по ФАМИЛИИ в поле "name" (у тех у кого она есть).
-def key_sorted_by_surname(obj_dict):
-    surname = re.findall(r'[A-Z]', obj_dict["name"])
-    surn = surname[-1]
-    return surn
-
-new_dict_list = sorted(dict_list, key=key_sorted_by_surname)
-print(new_dict_list)
+# def key_sorted_by_surname(obj_dict):
+#     surname = re.findall(r'[A-Z]', obj_dict["name"])
+#     surn = surname[-1]
+#     return surn
+#
+# new_dict_list = sorted(dict_list, key=key_sorted_by_surname)
+# print(new_dict_list)
 
 #### 3. Написать функцию сортировки по дате смерти из поля "years". ###########
 
 def key_sorted_by_data(obj_dict):
     years = re.findall(r'[0-9]+', obj_dict["years"])
-    death_year = years[-1]
-    years = "-".join(death_year) if "BC" in obj_dict["years"] else years
     d_data = years[-1]
+    d_data = -int(d_data) if "BC" in obj_dict["years"] else int(d_data)
     return int(d_data)
 
 new_dict_list = sorted(dict_list, key=key_sorted_by_data)
@@ -34,9 +33,9 @@ print(new_dict_list)
 
 #### 4. Написать функцию сортировки по количеству слов в поле "text". ###########
 
-def key_sorted_by_count_word(obj_dict):
-    list = re.findall(r" ", obj_dict["text"])
-    return len(list)
-
-new_dict_list = sorted(dict_list, key=key_sorted_by_count_word)
-print(new_dict_list)
+# def key_sorted_by_count_word(obj_dict):
+#     list = re.findall(r" ", obj_dict["text"])
+#     return len(list)
+#
+# new_dict_list = sorted(dict_list, key=key_sorted_by_count_word)
+# print(new_dict_list)
