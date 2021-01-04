f4a487e43ce788165757d48c945f419e8c5fae7f
ellen <alena.kushnir17@gmail.com>
2021-01-04 14:37:54 +0200

    Home work 5

--- HW10.py
+++ HW10.py
@@ -4,16 +4,16 @@ import json
 import re
 
 #### 1. Необходимо написать функцию, которая считает эти данные из файла. Параметр функции - имя файла. ###########
-def open_json_data():
-    with open("data.json", "r") as json_file:
+def open_json_data(filename="data.json"):
+    with open(filename, "r") as json_file:
         new_data = json.load(json_file)
-        return new_data
+    return new_data
 
 dict_list = open_json_data()
 
 # 2. Написать функцию сортировки данных по ФАМИЛИИ в поле "name" (у тех у кого она есть).
 def key_sorted_by_surname(obj_dict):
-    surname = re.findall(r'[A-Z]', obj_dict["name"])
+    surname = re.findall(r'[A-Z]+[a-z]+', obj_dict["name"])
     surn = surname[-1]
     return surn
 
@@ -34,8 +34,13 @@ print(new_dict_list)
 #### 4. Написать функцию сортировки по количеству слов в поле "text". ###########
 
 def key_sorted_by_count_word(obj_dict):
-    list = re.findall(r" ", obj_dict["text"])
-    return len(list)
+    text_list = (obj_dict["text"])
+    new_list= text_list.split(' ')
+    return len(new_list)
+#### Второй вариант: ###########
+def key_sorted_by_count_word(obj_dict):
+    new_list = re.findall(r" ", obj_dict["text"])
+    return len(new_list)
 
 new_dict_list = sorted(dict_list, key=key_sorted_by_count_word)
 print(new_dict_list)
