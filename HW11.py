a5fff3b5c735aa72f17a1274b46babf10e2c1dc5
ellen <alena.kushnir17@gmail.com>
2021-01-14 11:23:13 +0200

    Home work 5

--- HW11.py
+++ HW11.py
@@ -2,6 +2,7 @@
 import requests
 import json
 import re
+import csv
 
 
 # 1. Написать функцию, которая принимает в виде параметра целое число - количество цитат (см. урок 12).
@@ -11,34 +12,59 @@ import re
 # Author, Quote, URL. Если автор не указан, цитату не брать.
 # Перед сохранением в csv, записи отсортировать по автору (в алфавитном порядке).
 
+def get_quotes(count):
+    quote_dict_list = []
+    i = 0
+    url = "http://api.forismatic.com/api/1.0/"
 
-# url = "http://api.forismatic.com/api/1.0/"
-#
-# params = {"method": "getQuote",
-#           "format": "json",
-#           "key": 1,
-#           "lang": "ru"}
-# for i in range(10):
-#     params["key"] = i
-#     r = requests.get(url, params=params)
-#     quote = r.json()
-#     # print(quote)
-#     quote_text = quote["quoteText"]
-#     print(quote_text)
-#     quote_author = quote["quoteAuthor"]
-#     print(quote_author)
+    params = {"method": "getQuote",
+              "format": "json",
+              "key": 1,
+              "lang": "ru"}
+    while i != count:
+        params["key"] = i
+        r = requests.get(url, params=params)
+        quote = r.json()
+        quote_dict = {"Author": "", "Quote": "", "URL": ""}
+        if len(quote["quoteAuthor"]) > 0:
+            quote_dict["Author"] = quote["quoteAuthor"]
+            quote_dict["Quote"] = quote["quoteText"]
+            quote_dict["URL"] = quote["quoteLink"]
+            quote_dict_list.append(quote_dict)
+            i += 1
+    return quote_dict_list
 
+
+quotes = get_quotes(10)
+
+
+def key_sorted_by_name(quote):
+    return quote["Author"]
+
+
+quotes = sorted(quotes, key=key_sorted_by_name)
+
+
+def write_quotes_csv(filename="quotes.csv"):
+    with open(filename, "w", encoding="utf-8") as csv_file:
+        fieldnames = {"Author": "", "Quote": "", "URL": ""}
+        writer = csv.DictWriter(csv_file, fieldnames=fieldnames, delimiter=',')
+        writer.writeheader()
+        writer.writerows(quotes)
+
+
+write_quotes_csv()
 
 ###################################################################################################################
-# 2. Дан файл authors.txt
-# 2.1) написать функцию, которая считывает данные из этого файла,
-# возвращая СПИСОК тех строк в которых есть полная дата, писатель и указание на его день рождения или смерти.
-# Например: 26th February 1802 - Victor Hugo's birthday - author of Les Misérables.
+# # 2. Дан файл authors.txt
+# # 2.1) написать функцию, которая считывает данные из этого файла,
+# # возвращая СПИСОК тех строк в которых есть полная дата, писатель и указание на его день рождения или смерти.
+# # Например: 26th February 1802 - Victor Hugo's birthday - author of Les Misérables.
 def read_txt(filename="authors.txt"):
-    with open(filename, 'r') as file:
+    with open(filename, "r") as txt_file:
         data = []
-        for line in file.readlines():
-            if line.find("birthday") >= 0 or line.find("death") >= 0:
+        for line in txt_file.readlines():
+            if line.find("'s") >= 0 and (line.find("birthday") >= 0 or line.find("death") >= 0):
                 data.append(line)
     return data
 
@@ -46,29 +72,16 @@ def read_txt(filename="authors.txt"):
 list_autors = read_txt()
 
 
-###################################################################################################################
-
-# 2.2) Написать функцию, которая принимает список строк полученной в пункте 2.1, и возвращает список словарей
-# в формате {"name": name, "date": date},
-# где name это имя автора, а date - дата из строки в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
-# Например [{"name": "Charles Dickens", "date": "09/06/1870"}, ...,
-# {"name": "J. D. Salinger", "date": "01/01/1919"}]
-def list_to_dict(list_autors):
-    for autor in list_autors:
-        name = dict_names(autor)
-        date = [dict_data(autor)]
-        keys = ['name', 'date']
-        zipped = zip(name, date)
-        dicts_names_data = [dict(zip(keys, values)) for values in zipped]
-        print(dicts_names_data)
-    # return dicts_names_data
-
-
-# print(dicts_names_data)
-
+# ###################################################################################################################
+#
+# # 2.2) Написать функцию, которая принимает список строк полученной в пункте 2.1, и возвращает список словарей
+# # в формате {"name": name, "date": date},
+# # где name это имя автора, а date - дата из строки в формате "dd/mm/yyyy" (d-день, m-месяц, y-год)
+# # Например [{"name": "Charles Dickens", "date": "09/06/1870"}, ...,
+# # {"name": "J. D. Salinger", "date": "01/01/1919"}]
 
 def dict_names(autor):
-    old_dict_names = re.findall(r"[a-zA-Z. ]+[']", autor)
+    old_dict_names = re.findall(r"[A-Z][A-Za-z. ]+[']", autor)
     dict_names = [i.strip(' \'') for i in old_dict_names]
     return dict_names
 
@@ -101,17 +114,21 @@ def dict_data(autor):
     return result
 
 
-list_to_dict(list_autors)
+def list_to_dict(list_autors):
+    dict_list = []
+    for autor in list_autors:
+        author_dict = {"name":"", "date":""}
+        author_dict["name"] = ''.join(dict_names(autor))
+        author_dict["date"] = dict_data(autor)
+        dict_list.append(author_dict)
+    return dict_list
 
-# names = list_to_dict(name)
-# dates = data
-# def dict_names_data():
-#     keys = ['name', 'data']
-#     zipped = zip(name, data)
-#     dicts_names_data = [dict(zip(keys, values)) for values in zipped]
-#     return dicts_names_data
-#
-# print(dict_names_data())
 
-##################################################################################################################
-# 2.3) Написать функцию, которая сохраняет результат пункта 2.2 в json файл.
+# ##################################################################################################################
+# # 2.3) Написать функцию, которая сохраняет результат пункта 2.2 в json файл.
+
+def write_json(filename = "list_autors.json"):
+    with open(filename, "w") as write_in_json:
+        json.dump(list_to_dict(list_autors), write_in_json)
+
+write_json()
